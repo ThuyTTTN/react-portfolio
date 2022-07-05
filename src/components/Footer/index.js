75c038d6c2ccdff4bf03dab74752b67deaed15d3
Thuy Nguyen <ttruong06@gmail.com>
2022-07-04 22:51:08 -0400

    modified footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,23 +1,36 @@
 import React from "react";
 
+//import bootstrap styles
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 function Footer() {
   return (
-    <div className="main-footer">
-      <div className="container d-inline-flex">
-        <h6 className="footer-name">&copy; Thuy Nguyen 2022</h6>
-        <div className="footer-icons">
-          <a href="https://github.com/ThuyTTTN" className="p-3">
-            <i className="fa-brands fa-github"></i>
-          </a>
-          <a href="www.linkedin.com/in/thuy-nguyen-a2333031" className="p-3">
-            <i className="fa-brands fa-linkedin"></i>
-          </a>
-          <a href="/" className="p-3">
-            <i className="fa-brands fa-twitter"></i>
-          </a>
-        </div>
-      </div>
-    </div>
+    <footer className="main-footer p-3">
+      <Container>
+        <Row>
+          <Col sm={12} md={6} className="m-md-0">
+            <p className="m-md-0 copyright">&copy; 2022 Thuy Nguyen</p>
+          </Col>
+          <Col
+            sm={12}
+            md={6}
+            className="d-flex justify-content-md-end justify-content-center"
+          >
+            <a href="https://github.com/ThuyTTTN" >
+              <i className="fa-brands fa-github"></i>
+            </a>&nbsp;
+            <a href="www.linkedin.com/in/thuy-nguyen-a2333031" >
+              <i className="fa-brands fa-linkedin"></i>
+            </a>&nbsp;
+            <a href="/" >
+              <i className="fa-brands fa-twitter"></i>
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 }
 
