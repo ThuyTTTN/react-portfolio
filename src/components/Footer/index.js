751a1fe1d601f1d7845b70c549663fcd4fa7a477
Thuy Nguyen <ttruong06@gmail.com>
2022-07-09 11:00:19 -0400

    linked icons to external links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -16,14 +16,14 @@ function Footer() {
             md={6}
             className="d-flex justify-content-md-end justify-content-center"
           >
-            <a href="https://github.com/ThuyTTTN" >
+            <a href="https://github.com/ThuyTTTN" target="_blank" >
               <i className="fa-brands fa-github icon"></i>
             </a>&nbsp;
-            <a href="www.linkedin.com/in/thuy-nguyen-a2333031" >
+            <a href="https://www.linkedin.com/in/thuy-nguyen-a2333031" target="_blank">
               <i className="fa-brands fa-linkedin icon"></i>
             </a>&nbsp;
-            <a href="/" >
-              <i className="fa-brands fa-twitter icon"></i>
+            <a href="https://www.instagram.com/thuytttn" target="_blank" >
+              <i className="fa-brands fa-instagram icon"></i>
             </a>
           </Col>
         </Row>
