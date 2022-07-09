3b09065d8921cd962eb577ba58d24cdc348791ca
Thuy Nguyen <ttruong06@gmail.com>
2022-07-09 16:42:25 -0400

    debugging header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,40 +1,21 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
-function Header({setPage}) {
-// const [activeLink, setActiveLink] = useState('home');
-
-// const onUpdateActiveLink = (value) => {
-//   setActiveLink(value);
-// }
+function Header({ setPage }) {
 
 
   return (
-      <Navbar expand="lg">
-        <Container>
-          {/* <Navbar.Brand href="#home">Thuy Nguyen</Navbar.Brand>   */}
-          <Nav className="about-me ms-auto">
-            <Nav.Link href="/about" className= 'navbar-link' onClick={() => {
-              // onUpdateActiveLink('home')
-              setPage('about')
-              }}>About Me</Nav.Link>
-            <Nav.Link href="/portfolio" className='navbar-link'  onClick={() => {
-              // onUpdateActiveLink('portfolio')
-              setPage('portfolio')}}>
-                Portfolio</Nav.Link>
-            <Nav.Link href="/resume" className= 'navbar-link' onClick={() => {
-              // onUpdateActiveLink('resume')
-              setPage('resume')
-              }}>Resume</Nav.Link>
-            <Nav.Link href="/contact" className= 'navbar-link'  onClick={() =>{
-              //  onUpdateActiveLink('contact')
-               setPage('contact')
-               }}>Contact</Nav.Link>
-            
-          </Nav>
-        </Container>
-      </Navbar>
-    
+    <Navbar expand="lg">
+      <Container>
+        <Nav className="about-me ms-auto">
+          <Nav.Link href="/about" className="navbar-link" onClick={() => { setPage("about"); }}>About Me</Nav.Link>
+          
+          <Nav.Link href="/portfolio" className="navbar-link" onClick={() => { setPage("portfolio"); }} >Portfolio</Nav.Link>
+          <Nav.Link href="/resume"  className="navbar-link"  onClick={() => { setPage("resume"); }}>Resume</Nav.Link>
+          <Nav.Link  href="/contact"  className="navbar-link"  onClick={() => { setPage("contact"); }}>Contact</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
--- src/components/MainContainer/index.js
+++ src/components/MainContainer/index.js
@@ -12,6 +12,7 @@ function MainContainer({page, setPage}) {
 
   const currentPage = () => {
     
+
     if (page === "about") {
       return <About />;
     }
