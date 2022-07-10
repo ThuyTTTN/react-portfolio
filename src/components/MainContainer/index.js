19d42c5207f8884175da3ed396e28c7b32a16569
Thuy Nguyen <ttruong06@gmail.com>
2022-07-10 15:58:00 -0400

    fixed header links

--- src/components/MainContainer/index.js
+++ src/components/MainContainer/index.js
@@ -5,32 +5,46 @@ import Footer from "../Footer";
 import ContactForm from "../Contact";
 import Project from "../Project";
 import Resume from "../Resume";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
-function MainContainer({page, setPage}) {
-  const currentPage = () => {
-    
-    if (page === "about") {
-      return <About />;
-    }
-    if (page === "contact") {
-      return <ContactForm />;
-    }
-    if (page === "portfolio") {
-      return <Project />;
-    }
-    if (page === "resume") {
-      return <Resume />;
-    }
-    
-  };
+function MainContainer() {
   return (
-   <>
-   <Header setPage={setPage} />
-      {currentPage()}
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Project} />
+        <Route exact path="/resume" component={Resume} />
+        <Route exact path="/contact" component={ContactForm} />
+      </Switch>
       <Footer />
-   </>
+    </Router>
   );
 }
 
 export default MainContainer;
+
+// const currentPage = () => {
+
+//   if (page === "about") {
+//     return <About />;
+//   }
+//   if (page === "contact") {
+//     return <ContactForm />;
+//   }
+//   if (page === "portfolio") {
+//     return <Project />;
+//   }
+//   if (page === "resume") {
+//     return <Resume />;
+//   }
+
+// };
+// return (
+//  <>
+//  <Header setPage={setPage} />
+//     {currentPage()}
+//     <Footer />
+//  </>
+// );
